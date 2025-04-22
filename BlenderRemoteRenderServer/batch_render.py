9f5dc4ce2496a802b5dc1d94bea64fcbbdde0526
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2025-04-22 18:52:45 +0100

    remove useless if statement

--- BlenderRemoteRenderServer/batch_render.py
+++ BlenderRemoteRenderServer/batch_render.py
@@ -40,8 +40,7 @@ def render_frames(frame_start, frame_end):
     Scene = bpy.context.scene
     # Create export folder
     export_folder = os.path.dirname(Scene.render.frame_path(frame=0))
-    if not os.path.exists(export_folder):
-        os.makedirs(export_folder)
+    os.makedirs(export_folder, exist_ok=True)
 
     # Render each frame
     for frame in range(frame_start, frame_end+1):
